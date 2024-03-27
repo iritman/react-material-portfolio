ae369b3ae05843e3711d468d9008a50130111767
Naiem YF <iritman@gmail.com>
2024-03-27 21:59:13 +0330

    implement profile.jsx

--- src/components/profile/profile.jsx
+++ src/components/profile/profile.jsx
@@ -1,7 +1,28 @@
+import { Typography } from "@mui/material";
 import React from "react";
 
+import "./profile.css";
+import profile_image from "./../../assets/images/my-profile.png";
+
 const Profile = () => {
-  return <div>Profile...</div>;
+  return (
+    <div className="profile container_shadow">
+      <div className="profile_name">
+        <Typography className="name">Naiem</Typography>
+        <Typography className="title">Title</Typography>
+      </div>
+
+      <figure className="profile_image">
+        <img src={profile_image} alt="" />
+      </figure>
+
+      <div className="profile_information">
+        Insert Timeline
+        <br />
+        <button>download</button>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
