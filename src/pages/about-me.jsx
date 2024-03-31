ea3070c2f688c9049159aa70d6944b3d6d24220b
Naiem YF <iritman@gmail.com>
2024-03-31 17:29:17 +0330

    refactor about-me module by creating label control

--- src/pages/about-me.jsx
+++ src/pages/about-me.jsx
@@ -1,6 +1,19 @@
 import React from "react";
 import { Paper, Box, Typography } from "@mui/material";
 
+const Label = (props) => {
+  const { children } = props;
+  return (
+    <Typography
+      variant="body2"
+      sx={{ textAlign: "justify", textJustify: "inter-word" }}
+      {...props}
+    >
+      {children}
+    </Typography>
+  );
+};
+
 const AboutMe = () => {
   return (
     <Paper>
@@ -13,18 +26,14 @@ const AboutMe = () => {
         >
           About me
         </Typography>
-        <Box mt={4} />
-        <Typography
-          variant="body2"
-          sx={{ textAlign: "justify", textJustify: "inter-word" }}
-        >
+        <Label mt={4}>
           Hey there, I'm Naiem Yousefifard, a versatile freelancer with a
           passion for crafting seamless digital experiences. With over two
           decades of hands-on experience in the realm of web and mobile
           development, I bring a wealth of expertise to every project I
           undertake.
-        </Typography>
-        <Typography variant="body2" mt={2}>
+        </Label>
+        <Label mt={2}>
           As a seasoned full-stack developer and Faradars instructor, I thrive
           on tackling complex challenges and transforming ideas into reality. My
           skill set spans a wide range of technologies, including Node.js,
@@ -32,8 +41,8 @@ const AboutMe = () => {
           solutions with Express and MongoDB or crafting elegant frontend
           interfaces with Material UI and Antd UI, I'm committed to delivering
           high-quality, scalable solutions that exceed expectations.
-        </Typography>
-        <Typography variant="body2" mt={2}>
+        </Label>
+        <Label mt={2}>
           With a keen eye for detail and a deep understanding of modern
           development practices, I'm dedicated to staying at the forefront of
           industry trends and technologies. I take pride in my ability to
@@ -41,7 +50,7 @@ const AboutMe = () => {
           smooth communication and seamless project delivery. If you're looking
           for a reliable partner to bring your vision to life, I'm here to help.
           Let's create something extraordinary together.
-        </Typography>
+        </Label>
       </Box>
     </Paper>
   );
